31bb7cbe04e0f575286cdcda525dbcda7d5234b5
ibn51 <ibn51@DESKTOP-9DKGGOC>
2021-04-13 09:11:55 +0900

    second commit

---
+++ Test0413/src/Test/qwer.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class qwer {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello,Git");
+		System.out.println("두번째클래스");
+		
+	}
+
+}
---
+++ Test0413/src/Test/qwerasdf.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class qwerasdf {
+
+	public static void main(String[] args) {
+
+		System.out.println("두번째클래스");
+	}
+
+}
